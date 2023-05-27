67ea6c42c338c4c0fbf09da51528ba3563362c8b
Aiman <aiman.bscsf19@iba-suk.edu.pk>
2023-05-27 17:26:26 +0500

    Added Timer

---
+++ src/Timer.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+export default function Timer() {
+  const [timer, setTimer] = useState(10);
+  var id ;
+  function tick() {
+    if(timer==0){
+        console.log(id);
+        
+        clearInterval(id);
+        alert("Time's Up!")
+        
+
+    }
+    else{
+        setTimer(prevState => prevState-1);
+    }
+   
+    
+   // console.log("called")
+  }
+
+  useEffect(() => {
+    id = setInterval(() => {
+        tick();
+    }, 1000)},[])
+
+  return (
+    <>
+      <p>Timer</p>
+      <span>{timer}</span>
+    </>
+  );
+}
